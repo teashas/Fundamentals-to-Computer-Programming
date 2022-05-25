7a6adb602c29323e3a3929b82997969921b93367
Saima Teasha <74995287+teashas@users.noreply.github.com>
2022-05-25 14:20:37 -0700

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,30 @@
+# 2.1 Write s Python expression.
+# The average age of a group of kids
+# 17 are 9 years old,
+# 24 are 10 years old,
+# 21 are 11 years old,
+# 27 are 12 years old.
+17 + 24 + 21 + 27 / 4
+
+# 2.2 Write a Python expression for.
+# """The number of times 61 goes into 4356"""
+4356 // 61
+
+# 2.3 Write a Boolean Python Expression.
+# """ The 7th character of s is 'g' """
+# s = "goodbye"
+false
+
+# 2.4 Write a Boolean Expression
+# """ The next to last chararter of s is 'x' """
+# s = "goodbye"
+false
+
+#2.5 Write n assignment statements
+#""" assign to var fullname the concat of first middle and last with spaces """
+first = "John"
+middle ="Fitzgerald"
+last="Kennedy"
+
+var = first + ' ' + middle + ' ' + last
+
